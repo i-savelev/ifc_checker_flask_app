35993315fc53a43cfdbe6010e6da5656cdbc0642
i-savelev <ilyasavelyev98@mail.ru>
2025-05-04 20:01:48 +0300

    fix

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, session, s
 import os
 from ifchelper import Ifc_help
 
+
 app = Flask(__name__)
 my_secret_key = ''
-amvera_var = os.environ["AMVERA"]
+amvera_var = os.getenv("AMVERA", '0')
+path = ''
 if str(amvera_var) == "1":
     app.secret_key = os.environ['MY_SECRET_KEY']
+    path = '/data'
 else:
+    path = os.path.abspath(os.curdir)+'/data'
     app.secret_key = 'your_secret_key'
-app.add_url_rule('/data/<path:filename>', 
+app.add_url_rule(f'/{path}/<path:filename>', 
                  endpoint='/data', 
-                 view_func=lambda filename: send_from_directory('/data/', filename))
+                 view_func=lambda filename: send_from_directory(f'{path}', filename))
 
 def allowed_file(filename, extension):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() == extension
@@ -47,23 +51,24 @@ def upload_file():
                 
             filename1 = file1.filename
             filename2 = file2.filename
-            session['file1_path'] = f'/data/{filename1}'
-            session['file2_path'] = f'/data/{filename2}'
-            file1.save(f'/data/{filename1}')
-            file2.save(f'/data/{filename2}')
+            session['file1_path'] = f'{path}/{filename1}'
+            session['file2_path'] = f'{path}/{filename2}'
+            file1.save(f'{path}/{filename1}')
+            file2.save(f'{path}/{filename2}')
             file_names = f'{filename1}, {filename2}'
             session['files_names'] = file_names
     return redirect(url_for('index'))
 
 @app.route('/process', methods=['POST'])
 def process():
+    session['result_path'] = path
     session.pop('result', None)
     session.pop('html_file_name', None)
     if 'file1_path' not in session or 'file2_path' not in session:
         session['error'] = 'Сначала загрузите файлы'
         return redirect(url_for('index'))
     try:
-        result = Ifc_help.check_ifc_file(session['file1_path'], session['file2_path'], '/data/')
+        result = Ifc_help.check_ifc_file(session['file1_path'], session['file2_path'], f'{path}')
         session['html_file_name'] = result
         session['result'] = result 
         session.pop('error', None)
@@ -76,10 +81,10 @@ def process():
 
 @app.route('/download')
 def dowload():
-    UPLOAD_DIR = '/data/'
+    UPLOAD_DIR = f'{path}'
     return send_from_directory(UPLOAD_DIR, session['html_file_name'], as_attachment=True)
 
 
 if __name__ == '__main__':
     ...
-    # app.run(debug=True)
+    app.run(debug=True)
